cb399c2cd75470a746bbe28fe855e17c355de5ae
Lyaysan2 <70819092+Lyaysan2@users.noreply.github.com>
2023-03-08 13:53:54 +0300

    form fixed

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import main_view, registration_view, auth_view, logout_view, note_edit_view, tags_view, tags_delete_view
+from web.views import main_view, registration_view, auth_view, logout_view, note_edit_view, tags_view, tags_delete_view, \
+    note_delete_view
 
 urlpatterns = [
     path('', main_view, name='main'),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('notes/add/', note_edit_view, name='note_add'),
     path('notes/<int:id>/', note_edit_view, name='note_edit'),
+    path('notes/<int:id>/delete', note_delete_view, name='note_delete'),
     path('tags/', tags_view, name='tags'),
     path('tags/<int:id>/delete/', tags_delete_view, name='tags_delete'),
 ]
